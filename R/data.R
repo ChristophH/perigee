9136321231db9bbc8d7fccb5411bfe2de6f162f9
Christoph Hafemeister <christoph.hafemeister@nyu.edu>
2021-06-19 23:26:59 +0200

    Add pre-trained model for human lung

--- R/data.R
+++ R/data.R
@@ -1,9 +1,19 @@
 #' Pre-trained model for human PBMC
 #'
-#' Model trained on Human PBMC. 29 non-doublet cell types.
+#' Model trained on human PBMC. 29 non-doublet cell types.
 #' See https://azimuth.hubmapconsortium.org/references/#Human\%20-\%20PBMC (annotation level 2).
 #'
 #' @format A list - the output of Perigee::pg_train
 #'
 #' @source \url{https://azimuth.hubmapconsortium.org/references/#Human\%20-\%20PBMC}
 "pg_model_human_pbmc"
+
+#' Pre-trained model for human lung
+#'
+#' Model trained on human lung and circulating blood. 39 cell types.
+#' See https://azimuth.hubmapconsortium.org/references/#Human\%20-\%20Lung (annotation level 1).
+#'
+#' @format A list - the output of Perigee::pg_train
+#'
+#' @source \url{https://azimuth.hubmapconsortium.org/references/#Human\%20-\%20Lung}
+"pg_model_human_lung"
